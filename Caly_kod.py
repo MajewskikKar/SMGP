26fd1186bfeba8fd071c1e77fec8cb17558662fb
Jan Okraska <jan.okraska@cern.ch>
2022-10-12 16:13:17 +0200

    Add concatenating mechanism for multiple maps

--- Caly_kod.py
+++ Caly_kod.py
@@ -1,88 +1,105 @@
 
 import requests
 import cv2
+import numpy as np
 import os
 
+def combine_map():
+    images = []
+    for i in range(255,260):
+        images.append(crop_map(i))
 
-# downloading file
-numer_ark = input("wpisz numer arksza") # str(210)
-response = requests.get("http://bazadata.pgi.gov.pl/data/smgp/arkusze_skany/smgp0" + numer_ark + ".jpg")
+    vis = np.concatenate(images, axis=1)
+    # cv2.imshow('output', vis)
 
-file = open(numer_ark + ".png", "wb")
-file.write(response.content)
-file.close()
+    # The image is only displayed if we call this
+    # cv2.waitKey(0)
+    cv2.imwrite(os.path.join(os.getcwd(), "outputs", "result.jpg"), vis) 
 
-# splitting SMGP into 4 pieces (not currently used)
+def crop_map(numer):
+    numer_ark = str(numer)
+    # downloading file
+    # numer_ark = input("wpisz numer arksza") # str(210)
+    response = requests.get("http://bazadata.pgi.gov.pl/data/smgp/arkusze_skany/smgp0" + numer_ark + ".jpg")
 
-cwd = os.getcwd()
-path = os.path.join(cwd, numer_ark)+".png"
-image = cv2.imread(path)
-# (h, w) = image.shape[:2]
-# centerX, centerY = (w//2), (h//2)
+    file = open(numer_ark + ".png", "wb")
+    file.write(response.content)
+    file.close()
 
-# topLeft = image[0:centerY, 0:centerX]
-# topRight = image[0:centerY, centerX:w]
-# #bottomLeft = image[]
-# #bottomRight = image[]
-# #cv2.imshow("TopRight", topLeft)
-# #cv2.waitKey(0)
+    # splitting SMGP into 4 pieces (not currently used)
 
-corner_names = {
-    "lt": (1,1),
-    "rt": (0, 1),
-    "lb": (1, 0),
-    "rb": (0, 0)
-}
+    cwd = os.getcwd()
+    path = os.path.join(cwd, numer_ark)+".png"
+    image = cv2.imread(path)
+    # (h, w) = image.shape[:2]
+    # centerX, centerY = (w//2), (h//2)
 
-corner_cords = []
-large_image = cv2.imread(path)
+    # topLeft = image[0:centerY, 0:centerX]
+    # topRight = image[0:centerY, centerX:w]
+    # #bottomLeft = image[]
+    # #bottomRight = image[]
+    # #cv2.imshow("TopRight", topLeft)
+    # #cv2.waitKey(0)
 
-for corner, flags in corner_names.items():
-    path1 = os.path.join(cwd, (corner + "rog.jpg"))
-    method = cv2.TM_SQDIFF_NORMED # one of six possible methods
+    corner_names = {
+        "lt": (1,1),
+        "rt": (0, 1),
+        "lb": (1, 0),
+        "rb": (0, 0)
+    }
 
-    # Read the images from the file
-    small_image = cv2.imread(path1)
+    corner_cords = []
+    large_image = cv2.imread(path)
 
-    result = cv2.matchTemplate(small_image, large_image, method)
-    
-    # We want the minimum squared difference
-    mn,_,mnLoc,_ = cv2.minMaxLoc(result)
+    for corner, flags in corner_names.items():
+        path1 = os.path.join(cwd, (corner + "rog.jpg"))
+        method = cv2.TM_SQDIFF_NORMED # one of six possible methods
 
-    # Draw the rectangle:
-    # Extract the coordinates of our best match
-    MPx,MPy = mnLoc
+        # Read the images from the file
+        small_image = cv2.imread(path1)
 
-    # Step 2: Get the size of the template. This is the same size as the match.
-    trows,tcols = small_image.shape[:2]
+        result = cv2.matchTemplate(small_image, large_image, method)
+        
+        # We want the minimum squared difference
+        mn,_,mnLoc,_ = cv2.minMaxLoc(result)
 
-    # Step 3: Draw the rectangle on large_image
-    cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
+        # Draw the rectangle:
+        # Extract the coordinates of our best match
+        MPx,MPy = mnLoc
 
-    x_mult, y_mult = flags
-    temp_corner = (MPx+tcols*x_mult, MPy+trows*y_mult)
-    corner_cords.append(temp_corner)
+        # Step 2: Get the size of the template. This is the same size as the match.
+        trows,tcols = small_image.shape[:2]
 
-for corner in corner_cords:
-    cv2.circle(large_image, corner, 20, (255,0, 255), 28)
+        # Step 3: Draw the rectangle on large_image
+        cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
 
-print(corner_cords)
+        x_mult, y_mult = flags
+        temp_corner = (MPx+tcols*x_mult, MPy+trows*y_mult)
+        corner_cords.append(temp_corner)
 
-lt, rt, lb, rb = corner_cords
-# this doesn't work
-min_x, min_y = lt
-max_x, max_y = rb
+    for corner in corner_cords:
+        cv2.circle(large_image, corner, 20, (255,0, 255), 28)
 
-cropped_image = large_image[min_y:max_y, min_x:max_x]
+    # print(corner_cords)
 
-w = max_x - min_x 
-h = max_y - min_y 
+    lt, rt, lb, rb = corner_cords
+    # this doesn't work
+    min_x, min_y = lt
+    max_x, max_y = rb
 
-# Display the original image with the rectangle around the match.
+    cropped_image = large_image[min_y:min_y+4500, min_x:max_x]
 
-cv2.imshow('output', cropped_image)
-# cv2.imshow('output',large_image[min_x:min_x+w, min_y:min_y+h])
+    w = max_x - min_x 
+    h = max_y - min_y 
+    print(h)
 
-# The image is only displayed if we call this
-cv2.waitKey(0)
+    return cropped_image
+    # Display the original image with the rectangle around the match.
 
+    # cv2.imshow('output', cropped_image)
+    # cv2.imshow('output',large_image[min_x:min_x+w, min_y:min_y+h])
+
+    # The image is only displayed if we call this
+    # cv2.waitKey(0)
+
+combine_map()
